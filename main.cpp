96300ed19b8fe7c7e995018c3a0ebad1cf75b06b
wwwruru <60737838+wwwruru@users.noreply.github.com>
2022-06-20 17:04:12 +0300

    thread

--- main.cpp
+++ main.cpp
@@ -9,63 +9,89 @@
 #include <dirent.h>
 #include <vector>
 #include <string>
+#include <mutex>
 
 using namespace std;
 
 cv::CascadeClassifier fasecascad;
+mutex mu;
+bool progress = true;
+struct Pictures
+{
+    cv::Mat frame;
+    string name;
+};
 
-void LoadPictureName (string inpath, vector<vector<string>*> &name)
+vector<Pictures>  pictures;
+
+void Loadpicture (string inpath, vector<Pictures> &pictures, bool &progress)
 {
     DIR *path = opendir(inpath.c_str());
     struct dirent *dir;
     if (path)
     {
-        int i = 0;
         while ((dir = readdir(path)) != NULL)
         {
-            name[i]->push_back(dir->d_name);
-            i++;
-            if (i == name.size())
-                i = 0;
+            cv::Mat frame = cv::imread(inpath + "/" + dir->d_name);
+            if (frame.data)
+            {
+                mu.lock();
+                pictures.push_back(Pictures{frame, dir->d_name});
+                mu.unlock();
+            }
         }
     }
     else
     {
-        cout << "faund path"<< endl;
+        cout << "failed to open directory"<< endl;
     }
     closedir(path);
+    progress = false;
 }
 
 
 
-void DetectAndSave(string inpath, vector<string> *namefile, string outpath)
+void DetectAndSave(vector<Pictures> &pictures, string outpath)
 {
-    for (string name : *namefile)
+    while (progress || pictures.size() > 0)
     {
-        cout << name << endl;
-        cv::Mat frame = cv::imread(inpath + "/" + name );
-        if (frame.data)
+        Pictures pic;
+        mu.lock();
+        if (pictures.size() > 0)
         {
+            pic = pictures.back();
+            pictures.pop_back();
+         }
+         mu.unlock();
+         if (pic.frame.data)
+         {
+            cout <<  pictures.size() << "\t" << this_thread::get_id() << endl;
             cv::Mat frame_gray;
-            cv::cvtColor( frame, frame_gray, cv::COLOR_BGR2GRAY );
-            cv::equalizeHist( frame_gray, frame_gray );
+            cv::cvtColor(pic.frame, frame_gray, cv::COLOR_BGR2GRAY);
+            cv::equalizeHist(frame_gray, frame_gray);
 
             vector<cv::Rect> faces;
             fasecascad.detectMultiScale(frame_gray, faces);
-            for ( size_t i = 0; i < faces.size(); i++ )
-            {
-                cv::Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
-                cv::ellipse( frame, center, cv::Size(faces[i].width/2, faces[i].height/2),
-                                                      0, 0, 360, cv::Scalar(255, 0, 255), 4);
+            for (size_t i = 0; i < faces.size(); i++)
+                {
+                    cv::Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+                    cv::ellipse(pic.frame, center, cv::Size(faces[i].width/2, faces[i].height/2),
+                                                              0, 0, 360, cv::Scalar(255, 0, 255), 4);
 
-                cv::imwrite(outpath + "/" + name, frame);
-            }
+                    cv::imwrite(outpath + "/" + pic.name, pic.frame);
+                }
         }
     }
+
 }
 
 int main(int argc, char *argv[])
 {
+    cv::CommandLineParser parser(argc, argv,
+            "{-i|/home/kostya/Загрузки/dataset| path input }"
+            "{-o|/home/kostya/Загрузки/vivod| path output }"
+            "{-j|4 | number }");
+
     string  face_cascade_name = "/usr/share/opencv4/haarcascades/haarcascade_frontalface_alt2.xml";
     if( !fasecascad.load(face_cascade_name) )
     {
@@ -75,32 +101,17 @@ int main(int argc, char *argv[])
 
     vector <thread*> th;
 
-    string inpath = "/home/kostya/Загрузки/dataset";
-    string outpath = "/home/kostya/Загрузки/vivod";
-    int count_thread = 4;
+    string inpath = parser.get<string> ("-i");
+    string outpath = parser.get<string>("-o");
+    int count_thread = parser.get<int>("-j");
 
-    if(argc!=4) {
-            cout << "input path"<<"\n";
-            cin >> inpath;
-            cout << "output path"<<"\n";
-            cin >> outpath;
-            cout << "count thread"<<"\n";
-            cin >> count_thread;
-        }else
-        {
-            inpath =  argv[1];
-            outpath =  argv[2];
-            count_thread = atoi(argv[3]);
-        }
+    th.push_back(new thread (Loadpicture, inpath, ref(pictures), ref(progress)));
 
-    vector<vector<string>*> namefile;
-    for (int i = 0; i < count_thread; i++)
-        namefile.push_back(new vector<string>);
-    LoadPictureName(inpath, namefile);
+    cout <<  pictures.size() << endl;
 
-    for (int i = 0; i < count_thread; i++)
+    for (int i = 0; i < count_thread;i++)
     {
-        th.push_back(new thread (DetectAndSave, inpath, namefile[i], outpath));
+        th.push_back(new thread (DetectAndSave, ref(pictures), outpath));
     }
 
     for (thread* thr: th)
